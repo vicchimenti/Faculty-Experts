042c29f6f43afaafdf9470ba857162f73c2014a3
vicchimenti <victor.chimenti@gmail.com>
2021-03-29 10:20:04 -0700

    cm natrual height

--- profile-output-gridfeed.js
+++ profile-output-gridfeed.js
@@ -67,7 +67,7 @@
     var openCardFooter = '<div class="card-footer">';
     var closeCardFooter = '</div>';
     var disciplineString = "Athletics, Arts & Sciences, Business and Economics, Education, Law, Nursing, School of Theology and Ministry, Science and Engineering";
-    var titleLink = '<h3 class="card-title">' + firstName + ' ' + lastName + '</h3>';
+    var titleLink = '<h3 class="card-title"><a href="' + fullTextLink + '" title="Link to full bio of ' + firstName + ' ' + lastName + '">' + firstName + ' ' + lastName + '</a></h3>';
     var beginningHTML = '<div class="gridFeedItem profileItem card shadow col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-4" title="' + firstName + ' ' + lastName + '" id="id' + contentID + '" data-position-default="ZoneA" data-position-selected="ZoneA">';
     var endingHTML = '</div>';
 
@@ -160,9 +160,9 @@
 
         thumbNailString = '<span class="cardImageWrapper"><img src="' + primaryImage + '" class="card-img-top" alt="' + contentName + '" /></span>';
 
-        if (primaryImage.naturalWidth > primaryImage.naturalHeight) {
-            titleLink = '<h3 class="card-title"><a href="' + fullTextLink + '" title="Link to full bio of ' + firstName + ' ' + lastName + '">' + firstName + ' ' + lastName + '</a></h3>';
-        }
+        // if (primaryImage.naturalWidth < primaryImage.naturalHeight) {
+        //     titleLink = '<h3 class="card-title">' + firstName + ' ' + lastName + '</h3>';
+        // }
         
     }
 
