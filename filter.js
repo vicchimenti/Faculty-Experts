82929da44952080336efaf570c077abb3c496502
vicchimenti <victor.chimenti@gmail.com>
2022-09-26 15:19:48 -0700

    import dropdown populator

--- filter.js
+++ filter.js
@@ -46,6 +46,20 @@ input.forEach(function(el) {
 
 // document.getElementsByClassName('gridOrganizer').classList.add("classToBeAdded");
 
+
+var select = document.getElementById("selectNumber");
+var options = ["1", "2", "3", "4", "5"];
+
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+}
+<select id="selectNumber">
+    <option>Choose a number</option>
+</select>
  
 
 
