c03b98695e8e387f92c43213257d0c856fbcd1da
vicchimenti <victor.chimenti@gmail.com>
2022-09-27 08:06:05 -0700

    clean filters

--- filter.js
+++ filter.js
@@ -46,7 +46,6 @@ for(let i = 0; i < optionArr.length; i++) {
  
 
 $(function () {
-    // After the DOM is ready, Wait until the window loads
     $(window).load(function () {
         // Once window loads set a timeout delay
         setTimeout(function () {
@@ -64,14 +63,10 @@ $(function () {
             //   ***   Process and Parse Visible Items   ***   //
             $(function () {
                 let parseItemsToDisplay = function() {
-                    // assign array of currently visible content items
                     visibleItems = $('.profileItem').not('.hideByDropdownCategories, hideByText');
-                    // check to see if array is empty
                     if (visibleItems.length == 0) {
-                        // when array is empty show the results message
                         $('.noResultsToShow').removeClass('hideResultsMessage');
                     } else {
-                        // when array has content items suppress the results message
                         $('.noResultsToShow').addClass('hideResultsMessage');
                     }
                 };
@@ -83,18 +78,16 @@ $(function () {
             
             //  ***   Keyword Search   ***   //
             $(function () {
-                // scan the keyword each character the user inputs
+
                 $('#find-experts').on('keyup', function () {
-                    // Assign Search Key
+
                     let keyword = $(this).val().toLowerCase();
-                    // filter the items for the input key
                     $(function () {
                         $('.profileItem').filter(function () {
-                            // when the search key is not present in the item then hide the item
                             $(this).toggleClass('hideByText', !($(this).text().toLowerCase().indexOf(keyword) > -1));
                         });
                     });
-                    // parse out unselected content items and limit display to user selected items
+
                     parseItems.process();
                 });
             });
@@ -105,40 +98,25 @@ $(function () {
             //   ***   Category Filter   ***   //
             $(function () {
 
-                // let selectElement = document.getElementById('selectboxCategorieOptions');
-                // selectElement.addEventListener('change', (event) => {
-
-                // });
-
-
-                // When the Dropdown Menu Selector Course Types Change - Execute change function
                 $('#categoryOptions').change(function () {
-                    // document.getElementsByClassName('gridOrganizer').classList.add("classToBeAdded");
 
-                    // document.getElementById("div1").classList.add("classToBeAdded");
-                    // Assign Search Key
                     let typeKey = $(this).val();
-                    // If Search Key is Not Null then Compare to the Type List Items in Each Content Item
-                    // document.getElementsByClassName('profileItem').classList.remove("hideByDropdownCategories");
                     if (typeKey) {
-                        // search tags in each item
+
                         $('ul.list-group').filter(function (i, e) {
                             var typeValue = $(this).text();
-                            // Check to see if the Key and Value are a Match
                             if (typeValue.match(typeKey)) {
                                 $(this).parents('.profileItem').removeClass('hideByDropdownCategories');
                             } else {
                                 $(this).parents('.profileItem').addClass('hideByDropdownCategories');
                             }
                         });
-                        // Else the Search Key is Null so Reset all Content Items to Visible
+
                     } else {
-                        // let elements = document.querySelectorAll(".profileItem");
-                        // elements.classList.remove("hideByDropdownCategories");
-                        // document.getElementsByClassName("profileItem").classList.remove("hideByDropdownCategories");
+
                         $('.profileItem').removeClass('hideByDropdownCategories');
                     }
-                    // parse out unselected content items and limit display to user selected items
+
                     parseItems.process();
                 });
             });
